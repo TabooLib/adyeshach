6a18e2aead5590b6059e4148576151f632a90a79
Soldier__ <1224954468@qq.com>
2020-08-17 16:03:29 +0800

    add phantom init & getter setter

--- src/main/kotlin/ink/ptms/adyeshach/common/entity/type/AdyPhantom.kt
+++ src/main/kotlin/ink/ptms/adyeshach/common/entity/type/AdyPhantom.kt
@@ -6,4 +6,16 @@ import ink.ptms.adyeshach.common.entity.EntityTypes
  * @author sky
  * @date 2020/8/4 23:15
  */
-class AdyPhantom() : AdyEntityLiving(EntityTypes.PHANTOM)
+class AdyPhantom() : AdyEntityLiving(EntityTypes.PHANTOM) {
+    init {
+        registerMeta(at(11500 to 15, 11400 to 14, 11300 to 12), "size", 0)
+    }
+
+    fun setSize(size: Int) {
+        setMetadata("size", size)
+    }
+
+    fun getSize(): Int {
+        return getMetadata("size")
+    }
+}
