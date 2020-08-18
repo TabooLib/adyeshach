657f6c627b5b98fe2afd04ee99a38521fd98684b
Arasple <Arasple@163.com>
2020-08-18 21:28:34 +0800

    Update Panda, Parrot & Polar Bear

--- src/main/kotlin/ink/ptms/adyeshach/common/entity/type/AdyParrot.kt
+++ src/main/kotlin/ink/ptms/adyeshach/common/entity/type/AdyParrot.kt
@@ -1,9 +1,34 @@
 package ink.ptms.adyeshach.common.entity.type
 
 import ink.ptms.adyeshach.common.entity.EntityTypes
+import org.bukkit.entity.Parrot
+import org.bukkit.entity.Parrot.Variant.*
 
 /**
  * @author sky
  * @date 2020/8/4 23:15
  */
-class AdyParrot() : AdyEntityLiving(EntityTypes.PARROT)
+class AdyParrot() : AdyEntityLiving(EntityTypes.PARROT) {
+
+    init {
+        registerMeta(at(11500 to 18, 11400 to 17, 11200 to 15), "color", 0)
+    }
+
+    fun setColor(color: Parrot.Variant) {
+        setMetadata(
+            "color",
+            when (color) {
+                RED -> 0
+                BLUE -> 1
+                GREEN -> 2
+                CYAN -> 3
+                GRAY -> 4
+            }
+        )
+    }
+
+    fun getColor(): Parrot.Variant {
+        return values()[getMetadata("color")]
+    }
+
+}
--- src/main/kotlin/ink/ptms/adyeshach/common/entity/type/AdyPolarBear.kt
+++ src/main/kotlin/ink/ptms/adyeshach/common/entity/type/AdyPolarBear.kt
@@ -6,4 +6,18 @@ import ink.ptms.adyeshach.common.entity.EntityTypes
  * @author sky
  * @date 2020/8/4 23:15
  */
-class AdyPolarBear() : AdyEntityAgeable(EntityTypes.POLAR_BEAR)
+class AdyPolarBear() : AdyEntityAgeable(EntityTypes.POLAR_BEAR) {
+
+    init {
+        registerMeta(at(11500 to 16, 11400 to 15, 11000 to 13), "isStanding", false)
+    }
+
+    fun isStanding(): Boolean {
+        return getMetadata("isStanding")
+    }
+
+    fun setStanding(value: Boolean) {
+        setMetadata("isStanding", value)
+    }
+
+}
