51ec7b192316b4fad47735bfc5392c68f16a1269
Arasple <Arasple@163.com>
2020-08-22 21:23:54 +0800

    Update SmoothLook

--- src/main/kotlin/ink/ptms/adyeshach/TestV.kt
+++ src/main/kotlin/ink/ptms/adyeshach/TestV.kt
@@ -4,6 +4,7 @@ import ink.ptms.adyeshach.api.AdyeshachAPI
 import ink.ptms.adyeshach.common.entity.EntityTypes
 import ink.ptms.adyeshach.common.entity.ai.general.GeneralGravity
 import ink.ptms.adyeshach.common.entity.ai.general.GeneralMove
+import ink.ptms.adyeshach.common.entity.ai.general.GeneralSmoothLook
 import ink.ptms.adyeshach.common.util.Tasks
 import io.izzel.taboolib.module.command.lite.CommandBuilder
 import io.izzel.taboolib.module.inject.TInject
@@ -28,11 +29,14 @@ object TestV {
 
             entity.pathfinder.add(GeneralMove(entity))
             entity.pathfinder.add(GeneralGravity(entity))
+            entity.pathfinder.add(GeneralSmoothLook(entity))
 
             Tasks.delay(40, true) {
                 entity.controllerMove(sender.location)
+                entity.controllerLook(sender.location)
             }
-            Tasks.delay(100, true) {
+
+            Tasks.delay(300, true) {
                 entity.delete()
             }
             sender.sendMessage("done. ${System.currentTimeMillis() - time}")
--- src/main/kotlin/ink/ptms/adyeshach/common/entity/ai/general/GeneralSmoothLook.kt
+++ src/main/kotlin/ink/ptms/adyeshach/common/entity/ai/general/GeneralSmoothLook.kt
@@ -2,6 +2,8 @@ package ink.ptms.adyeshach.common.entity.ai.general
 
 import ink.ptms.adyeshach.common.entity.EntityInstance
 import ink.ptms.adyeshach.common.entity.ai.Pathfinder
+import ink.ptms.adyeshach.common.util.Tasks
+import org.bukkit.scheduler.BukkitTask
 
 /**
  * 实体平滑视角改变
@@ -14,11 +16,31 @@ class GeneralSmoothLook(entity: EntityInstance) : Pathfinder(entity) {
     var yaw = 0f
     var pitch = 0f
     var isLooking = false
+    var speed = 25
+    var task: BukkitTask? = null
 
     override fun shouldExecute(): Boolean {
         return isLooking
     }
 
     override fun onTick() {
+        if (shouldExecute() && task == null) {
+            val deltaYaw = (yaw - entity.position.yaw) / speed
+            val deltaPitch = (pitch - entity.position.pitch) / speed
+
+            println("SmoothLook: {${entity.position.yaw}, ${entity.position.pitch}} to {$yaw, $pitch} with Delta: {$deltaYaw, $deltaPitch}")
+
+            task = Tasks.timer(0, 1, true) {
+
+                entity.setHeadRotation(entity.position.yaw + deltaYaw, entity.position.pitch + deltaPitch)
+            }
+
+            Tasks.delay(speed.toLong(), true) {
+                isLooking = false
+                entity.setHeadRotation(yaw, pitch)
+                task?.cancel()
+                task = null
+            }
+        }
     }
 }
