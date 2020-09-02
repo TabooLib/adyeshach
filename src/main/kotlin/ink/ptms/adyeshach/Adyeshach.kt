6370433e353f198a818c51339023f1ffcd117854
sky <sky@asgard.me>
2020-09-02 23:31:57 +0800

    Update version check

--- src/main/kotlin/ink/ptms/adyeshach/Adyeshach.kt
+++ src/main/kotlin/ink/ptms/adyeshach/Adyeshach.kt
@@ -5,8 +5,10 @@ import ink.ptms.adyeshach.common.script.ScriptHandler
 import io.izzel.taboolib.Version
 import io.izzel.taboolib.loader.Plugin
 import io.izzel.taboolib.loader.PluginBase
+import io.izzel.taboolib.module.command.lite.CommandBuilder
 import io.izzel.taboolib.module.config.TConfig
 import io.izzel.taboolib.module.inject.TInject
+import org.bukkit.Bukkit
 
 object Adyeshach : Plugin() {
 
@@ -20,9 +22,17 @@ object Adyeshach : Plugin() {
     val scriptHandler = ScriptHandler
 
     override fun onLoad() {
-        if (Version.isBefore(Version.v1_9)) {
+        if (PluginBase.isForge() || Version.isBefore(Version.v1_9)) {
             PluginBase.setDisabled(true)
-            println("[Adyeshach] Sorry, The Adyeshach not supported this minecraft version.")
+            Bukkit.getConsoleSender().sendMessage("§4[Adyeshach] §cSorry, The Adyeshach not supported this minecraft version.")
+            try {
+                CommandBuilder.create("adyeshach", plugin)
+                        .aliases("anpc", "npc")
+                        .execute { sender, _ ->
+                            sender.sendMessage("§4[Adyeshach] §cSorry, The Adyeshach not supported this minecraft version.")
+                        }.build()
+            } catch (t: Throwable) {
+            }
         }
     }
 
