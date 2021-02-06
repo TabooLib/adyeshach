12a424ef32ab92266fdc0eceaf6aee2560c1e8a7
sky <sky@asgard.me>
2021-02-06 23:46:10 +0800

    update loader 2.11

--- src/main/kotlin/ink/ptms/adyeshach/Adyeshach.kt
+++ src/main/kotlin/ink/ptms/adyeshach/Adyeshach.kt
@@ -4,7 +4,7 @@ import ink.ptms.adyeshach.api.Settings
 import ink.ptms.adyeshach.common.script.ScriptHandler
 import io.izzel.taboolib.Version
 import io.izzel.taboolib.loader.Plugin
-import io.izzel.taboolib.loader.PluginBase
+import io.izzel.taboolib.loader.PluginBoot
 import io.izzel.taboolib.module.command.lite.CommandBuilder
 import io.izzel.taboolib.module.config.TConfig
 import io.izzel.taboolib.module.inject.TInject
@@ -22,8 +22,8 @@ object Adyeshach : Plugin() {
     val scriptHandler = ScriptHandler
 
     override fun onLoad() {
-        if (PluginBase.isForge() || Version.isBefore(Version.v1_9)) {
-            PluginBase.setDisabled(true)
+        if (PluginBoot.isForge() || Version.isBefore(Version.v1_9)) {
+            PluginBoot.setDisabled(true)
             Bukkit.getConsoleSender().sendMessage("§4[Adyeshach] §cSorry, The Adyeshach not supported this minecraft version.")
             try {
                 CommandBuilder.create("adyeshach", plugin)
