01c68e4f97aee82163228127ec61360bb4e49476
坏黑 <b@skymc.me>
2021-11-01 22:36:56 +0800

    [1.4.2] Update "view-condition" trait

---
+++ src/main/kotlin/ink/ptms/adyeshach/internal/trait/impl/TraitViewCondition.kt
@@ -0,0 +1,56 @@
+package ink.ptms.adyeshach.internal.trait.impl
+
+import ink.ptms.adyeshach.api.event.AdyeshachEntityRemoveEvent
+import ink.ptms.adyeshach.api.event.AdyeshachEntityVisibleEvent
+import ink.ptms.adyeshach.common.entity.EntityInstance
+import ink.ptms.adyeshach.common.util.Inputs.inputBook
+import ink.ptms.adyeshach.internal.trait.Trait
+import org.bukkit.entity.Player
+import taboolib.common.platform.event.SubscribeEvent
+import taboolib.common.platform.function.adaptPlayer
+import taboolib.common5.Coerce
+import taboolib.module.kether.KetherShell
+import taboolib.module.kether.printKetherErrorMessage
+import taboolib.platform.util.sendLang
+
+object TraitViewCondition : Trait() {
+
+    @SubscribeEvent
+    fun e(e: AdyeshachEntityRemoveEvent) {
+        data.set(e.entity.uniqueId, null)
+    }
+
+    @SubscribeEvent
+    fun e(e: AdyeshachEntityVisibleEvent) {
+        if (e.visible && data.contains(e.entity.uniqueId)) {
+            try {
+                KetherShell.eval(data.getStringList(e.entity.uniqueId), sender = adaptPlayer(e.viewer)).thenAccept {
+                    if (Coerce.toBoolean(it)) {
+                        return@thenAccept
+                    }
+                    e.isCancelled = true
+                }
+            } catch (ex: Exception) {
+                ex.printKetherErrorMessage()
+            }
+        }
+    }
+
+    override fun getName(): String {
+        return "view-condition"
+    }
+
+    override fun edit(player: Player, entityInstance: EntityInstance) {
+        player.sendLang("trait-condition")
+        player.inputBook(data.getStringList(entityInstance.uniqueId)) {
+            if (it.all { line -> line.isBlank() }) {
+                data.set(entityInstance.uniqueId, null)
+            } else {
+                data.set(entityInstance.uniqueId, it)
+            }
+            entityInstance.destroy()
+            entityInstance.respawn()
+            player.sendLang("trait-condition-finish")
+        }
+    }
+}
