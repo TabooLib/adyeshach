dbaf329f678e8c9a5d92a30bbdbaa0b58861e303
坏黑 <b@skymc.me>
2021-12-11 22:28:57 +0800

    [1.4.17] Supported 1.18.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     `java-library`
     `maven-publish`
-    id("io.izzel.taboolib") version "1.31"
+    id("io.izzel.taboolib") version "1.32"
     id("org.jetbrains.kotlin.jvm") version "1.5.10"
     id("org.jetbrains.dokka") version "1.5.30"
 }
@@ -37,7 +37,7 @@ taboolib {
     install("platform-bukkit")
     install("expansion-command-helper")
     classifier = null
-    version = "6.0.6-22"
+    version = "6.0.6-24"
 }
 
 repositories {
