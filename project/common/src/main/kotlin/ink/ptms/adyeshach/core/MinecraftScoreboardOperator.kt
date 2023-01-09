d64863759a2f689f18e9256341f7ab3d9b8260d2
坏黑 <b@skymc.me>
2023-01-10 02:47:21 +0800

    [2.0][dev] Update

--- project/common/src/main/kotlin/ink/ptms/adyeshach/core/MinecraftScoreboardOperator.kt
+++ project/common/src/main/kotlin/ink/ptms/adyeshach/core/MinecraftScoreboardOperator.kt
@@ -25,11 +25,104 @@ interface MinecraftScoreboardOperator {
     }
 
     /**
+     * 创建虚拟队伍
+     *
+     * @param player 数据包接收人
+     * @param name 队伍名称
+     * @param members 队伍成员
+     * @param nameTagVisible 名称可见
+     * @param collision 是否碰撞
+     * @param color 队伍颜色
+     */
+    fun createTeam(
+        player: List<Player>,
+        name: String,
+        members: List<String>,
+        nameTagVisible: Boolean = true,
+        collision: Boolean = true,
+        color: ChatColor = ChatColor.WHITE
+    ) {
+        updateTeam(player, Team(name, members.toMutableList(), nameTagVisible, collision, color), TeamMethod.ADD)
+    }
+
+    fun createTeam(
+        player: Player,
+        name: String,
+        members: List<String>,
+        nameTagVisible: Boolean = true,
+        collision: Boolean = true,
+        color: ChatColor = ChatColor.WHITE
+    ) {
+        updateTeam(listOf(player), Team(name, members.toMutableList(), nameTagVisible, collision, color), TeamMethod.ADD)
+    }
+
+    /**
+     * 使实体加入虚拟队伍
+     *
+     * @param player 数据包接收人
+     * @param name 队伍名称
+     * @param members 队伍成员
+     */
+    fun joinTeam(player: List<Player>, name: String, members: List<String>) {
+        updateTeam(player, Team(name, members.toMutableList()), TeamMethod.JOIN)
+    }
+
+    fun joinTeam(player: Player, name: String, members: List<String>) {
+        updateTeam(listOf(player), Team(name, members.toMutableList()), TeamMethod.JOIN)
+    }
+
+    /**
+     * 使实体离开虚拟队伍
+     *
+     * @param player 数据包接收人
+     * @param name 队伍名称
+     * @param members 队伍成员
+     */
+    fun leaveTeam(player: List<Player>, name: String, members: List<String>) {
+        updateTeam(player, Team(name, members.toMutableList()), TeamMethod.LEAVE)
+    }
+
+    fun leaveTeam(player: Player, name: String, members: List<String>) {
+        updateTeam(listOf(player), Team(name, members.toMutableList()), TeamMethod.LEAVE)
+    }
+
+    /**
+     * 删除虚拟队伍
+     *
+     * @param player 数据包接收人
+     * @param name 队伍名称
+     */
+    fun removeTeam(player: List<Player>, name: String) {
+        updateTeam(player, Team(name), TeamMethod.REMOVE)
+    }
+
+    fun removeTeam(player: Player, name: String) {
+        updateTeam(listOf(player), Team(name), TeamMethod.REMOVE)
+    }
+
+    /**
+     * 修改虚拟队伍信息
+     *
+     * @param player 数据包接收人
+     * @param name 队伍名称
+     * @param nameTagVisible 名称可见
+     * @param collision 是否碰撞
+     * @param color 队伍颜色
+     */
+    fun modifyTeam(player: Player, name: String, nameTagVisible: Boolean = true, collision: Boolean = true, color: ChatColor = ChatColor.WHITE) {
+        updateTeam(listOf(player), Team(name, nameTagVisible = nameTagVisible, collision = collision, color = color), TeamMethod.CHANGE)
+    }
+
+    fun modifyTeam(player: List<Player>, name: String, nameTagVisible: Boolean = true, collision: Boolean = true, color: ChatColor = ChatColor.WHITE) {
+        updateTeam(player, Team(name, nameTagVisible = nameTagVisible, collision = collision, color = color), TeamMethod.CHANGE)
+    }
+
+    /**
      * 队伍信息
      */
     data class Team(
         var name: String,
-        var members: ArrayList<String> = arrayListOf(),
+        var members: MutableList<String> = arrayListOf(),
         var nameTagVisible: Boolean = true,
         var collision: Boolean = true,
         var color: ChatColor = ChatColor.WHITE
