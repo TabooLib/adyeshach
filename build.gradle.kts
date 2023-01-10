ca5843771329a12fef43b6b8795feb19f50e4822
坏黑 <b@skymc.me>
2023-01-10 21:17:17 +0800

    [2.0][dev] Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,67 +59,10 @@ subprojects {
             freeCompilerArgs = listOf("-Xjvm-default=all", "-Xextended-compiler-checks")
         }
     }
-    <<<<<<< Updated upstream
     configure<JavaPluginConvention> {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    =======
-    install("common")
-    install("common-5")
-    install("module-database")
-    install("module-database-mongodb")
-    install("module-configuration")
-    install("module-kether")
-    install("module-chat")
-    install("module-lang")
-    install("module-metrics")
-    install("module-navigation")
-    install("module-nms")
-    install("module-nms-util")
-    install("module-ui")
-    install("platform-bukkit")
-    install("expansion-command-helper")
-    classifier = null
-    version = "6.0.10-31"
-}
-
-repositories {
-    mavenLocal()
-    mavenCentral()
-}
-
-dependencies {
-    compileOnly("ink.ptms:Zaphkiel:1.7.0")
-    compileOnly("public:ServerTours:1.6.5")
-    compileOnly("public:ModelEngine:2.5.1")
-    compileOnly("public:GermPlugin:4.0.3")
-    compileOnly("net.citizensnpcs:Citizens:2.0.18")
-    compileOnly("com.guillaumevdn.gcore:GCore:7.15.0")
-    compileOnly("com.guillaumevdn.questcreator:QuestCreator:1")
-    compileOnly("com.isnakebuzz.servernpc:ServerNPC:1")
-    compileOnly("org.betonquest:betonquest:2.0.0")
-    compileOnly("pl.betoncraft:betonquest:1.12.5")
-    compileOnly("ink.ptms.core:v11903:11903:mapped")
-    compileOnly("ink.ptms.core:v11903:11903:universal")
-    compileOnly("ink.ptms.core:v11902:11902:mapped")
-    compileOnly("ink.ptms.core:v11901:11901:mapped")
-    compileOnly("ink.ptms.core:v11900:11900:mapped")
-    compileOnly("ink.ptms.core:v11800:11800:mapped")
-    compileOnly("ink.ptms.core:v11701:11701:mapped")
-    compileOnly("ink.ptms.core:v11604:11604")
-    compileOnly("ink.ptms.core:v11600:11600")
-    compileOnly("ink.ptms.core:v11500:11500")
-    compileOnly("ink.ptms.core:v11400:11400")
-    compileOnly("ink.ptms.core:v11300:11300")
-    compileOnly("ink.ptms.core:v11200:11200")
-    compileOnly("ink.ptms.core:v11100:11100")
-    compileOnly("ink.ptms.core:v11000:11000")
-    compileOnly("ink.ptms.core:v10900:10900")
-    compileOnly(kotlin("stdlib"))
-    taboo(fileTree("libs"))
-    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.6.0")
-    >>>>>>> Stashed changes
 }
 
 gradle.buildFinished {
