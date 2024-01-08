9bf4456f93252ea24c9a8920fe887b75fa8b2606
Bkm016 <sky@asgard.me>
2024-01-08 23:02:01 +0800

    Update "rename" action

---
+++ project/common-impl/src/main/kotlin/ink/ptms/adyeshach/impl/script/ActionRename.kt
@@ -0,0 +1,22 @@
+package ink.ptms.adyeshach.impl.script
+
+import ink.ptms.adyeshach.core.util.errorBy
+import ink.ptms.adyeshach.impl.getEntities
+import ink.ptms.adyeshach.impl.getManager
+import ink.ptms.adyeshach.impl.isEntitySelected
+import taboolib.module.kether.KetherParser
+import taboolib.module.kether.combinationParser
+import taboolib.module.kether.script
+
+@KetherParser(["animation"], namespace = "adyeshach", shared = true)
+private fun actionName() = combinationParser {
+    it.group(text()).apply(it) { newId ->
+        now {
+            val script = script()
+            if (script.getManager() == null || !script.isEntitySelected()) {
+                errorBy("error-no-manager-or-entity-selected")
+            }
+            script.getEntities().forEach { e -> e.id = newId }
+        }
+    }
+}
