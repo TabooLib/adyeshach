1059282595c172fcc2ee320ffc6bcaf55ba16dc4
Bkm016 <sky@asgard.me>
2024-03-16 17:36:31 +0800

    更新 tb 版本,

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ subprojects {
         env {
             install(UNIVERSAL, DATABASE, EFFECT, NMS_UTIL, KETHER, UI, NAVIGATION, METRICS, BUKKIT_ALL)
         }
-        version { taboolib = "6.1.1-beta10" }
+        version { taboolib = "6.1.1-beta11" }
     }
     repositories {
         maven { url = uri("https://repo.spongepowered.org/maven") }
