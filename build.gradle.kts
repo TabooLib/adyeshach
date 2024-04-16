3175d0276ce17ed1649e12276c848f0c6b1d7314
Bkm016 <sky@asgard.me>
2024-04-17 02:54:18 +0800

    update taboolib

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ subprojects {
         env {
             install(UNIVERSAL, DATABASE, EFFECT, NMS_UTIL, KETHER, UI, NAVIGATION, METRICS, BUKKIT_ALL)
         }
-        version { taboolib = "6.1.1-beta24" }
+        version { taboolib = "6.1.1-beta27" }
     }
     repositories {
         maven { url = uri("https://repo.spongepowered.org/maven") }
