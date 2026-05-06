a3bc5c8bc6fc7ae3d5adbfe640f6972b4e8b7f12
黑 <b@taboo.sh>
2026-05-06 20:17:35 +0800

    fix(modelengine): 适配动画 priority 参数

--- project/compat-model-engine-v4/src/main/kotlin/ink/ptms/adyeshach/compat/modelengine4/ActionModelEngine.kt
+++ project/compat-model-engine-v4/src/main/kotlin/ink/ptms/adyeshach/compat/modelengine4/ActionModelEngine.kt
@@ -11,8 +11,8 @@ import taboolib.common.platform.Awake
 import taboolib.module.kether.*
 
 /**
- * modelengine animation add {token} [speed {double} [lerpin {int} [lerpout {int}]]]
- * modelengine animation remove {token} [ingorelerp {boolean}]
+ * modelengine animation add {token} [speed {double} [lerpin {int} [lerpout {int} [priority {int}]]]]
+ * modelengine animation remove {token} [ingorelerp {boolean} [priority {int}]]
  */
 @Awake(LifeCycle.LOAD)
 internal fun init() {
@@ -28,8 +28,9 @@ internal fun init() {
                 command("ingorelerp", then = bool()).option().defaultsTo(false), // ingorelerp {boolean}
                 command("force-change", then = bool()).option().defaultsTo(false), // force-change {boolean}
                 command("force-override", then = bool()).option().defaultsTo(false), // force-override {boolean}
-                command("loop", then = text()).option().defaultsTo("once") // loop {text}
-            ).apply(it) { action, method, token, speed, lerpin, lerpout, ingorelerp, forceChange, forceOverride, loop ->
+                command("loop", then = text()).option().defaultsTo("once"), // loop {text}
+                command("priority", then = int()).option().defaultsTo(1) // priority {int}
+            ).apply(it) { action, method, token, speed, lerpin, lerpout, ingorelerp, forceChange, forceOverride, loop, priority ->
                 now {
                     if (script().getManager() == null || !script().isEntitySelected()) {
                         script().throwUndefinedError()
@@ -43,11 +44,26 @@ internal fun init() {
                                     when (method.lowercase()) {
                                         // 添加
                                         "add", "play" -> {
-                                            e.playAnimation(e.modelEngineName, token, lerpin, lerpout, speed, forceChange, forceOverride, loopMode)
+                                            e.playAnimation(
+                                                modelId = e.modelEngineName,
+                                                animationId = token,
+                                                lerpIn = lerpin,
+                                                lerpOut = lerpout,
+                                                speed = speed,
+                                                isForceChange = forceChange,
+                                                isForceOverride = forceOverride,
+                                                loopMode = loopMode,
+                                                priority = priority
+                                            )
                                         }
                                         // 删除
                                         "remove" -> {
-                                            e.stopAnimation(e.modelEngineName, token, ingorelerp)
+                                            e.stopAnimation(
+                                                modelId = e.modelEngineName,
+                                                animationId = token,
+                                                ignoreLerp = ingorelerp,
+                                                priority = priority
+                                            )
                                         }
                                         // 其他
                                         else -> error("Unknown method: $method (play, remove)")
